a6a4a0930cf1c4a7722797c12eeb4e4166b4c66f
Erin Kim <erinjkim@hawaii.edu>
2025-03-23 21:23:49 -1000

    Transparency enabled

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,8 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 -- config.color_scheme = "Palenight (Gogh)"
-config.color_scheme = "PaleNightHC"
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Tokyo Night Moon"
 
 config.font = wezterm.font("Liga SFMono Nerd Font")
 -- config.font = wezterm.font("MesloLGS Nerd Font Mono")
@@ -24,10 +25,10 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
--- config.window_background_opacity = 0.9
--- config.macos_window_background_blur = 10
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 10
 
-config.window_background_opacity = 1
+-- config.window_background_opacity = 1
 
 -- and finally, return the configuration to wezterm
 return config
